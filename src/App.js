884337e548aedb2a3295b6e386a9d7533ffd2c44
Jiacheng-Hu <hu.jiac@northeastern.edu>
2020-10-12 01:34:31 +0800

    javascript-assignment

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       // put data fetching code here!
+      const response = await fetch("http://demo1390455.mockable.io/articles");
+      const responseJson = await response.json();
+      setFetchedData(responseJson);
+      console.log("RESPONSE", responseJson);
     };
 
     if (isEmpty(fetchedData)) {
--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -4,8 +4,14 @@ import HTMLText from "../HTMLText/HTMLText";
 const DynamicArticle = (props) => {
   return (
     <article>
-      {/* this is an example of how you insert props into a react component */}
-      <div>{props.article.title}</div>
+    	<header>
+    		<h1>{props.article.title}</h1>
+    		<address>
+    			by&nbsp;{props.article.author}&nbsp;(<a href="url">{props.article.authorEmail}</a>)
+    		</address>
+    		<time datetime={props.article.timeStamp}>{props.article.displayDate}</time>
+    	</header>
+      
       <HTMLText text={props.article.text} />
     </article>
   );
